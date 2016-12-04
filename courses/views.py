7eb73bb0929d943c347a68978479fc32a4c7b455
johnhu <huzichunjohn@126.com>
2016-12-04 14:41:27 +0800

    display course information.

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,14 @@
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
+from django.db.models import Count
 from braces.views import LoginRequiredMixin, PermissionRequiredMixin, CsrfExemptMixin, JsonRequestResponseMixin
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 
 class OwnerMixin(object):
@@ -161,3 +163,23 @@ class ContentOrderView(CsrfExemptMixin,
                     module__course__owner=request.user) \
                     .update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+                    total_courses=Count('courses'))
+        courses = Course.objects.annotate(
+                    total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
